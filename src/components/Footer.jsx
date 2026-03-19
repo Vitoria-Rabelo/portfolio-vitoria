23420a6159499b3e8c4e8e1426d039068a51abf2
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2026-03-19 13:07:18 -0300

    ft: create Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+        <div className="container p-12 flex justify-between font-bold text-pink-500">
+            <span>
+                &lt;Vitória Rabelo/&gt;
+            </span>
+            <p className="text-slate-600">
+                Copyright © 2026 Vitória Rabelo Santiago. All rights reserved.
+            </p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
