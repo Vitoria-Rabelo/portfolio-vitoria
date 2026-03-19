36e92abfd66411a27d88e05b462a1b296fcb3b5a
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2026-03-19 17:28:02 -0300

    ft: create knowledgesSection

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import AboutSession from "@/components/AboutSession";
 import ProjectsSection from "@/components/ProjectsSection";
 import EmailSection from "@/components/EmailSection";
 import Footer from "@/components/Footer";
+import KnowledgesSection from "@/components/KnowledgesSection";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <div className="container mx-auto px-12 py-4">
         <HeroSession/>
         <AboutSession/>
+        <KnowledgesSection/>
         <ProjectsSection/>
         <EmailSection/>
       </div>
