5589945d007573d969b36f84bfa357e4038564e9
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2026-03-19 17:58:04 -0300

    ft: create colors pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ export default {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+     colors: {
+      ...colors,
+      primary: colors.purple,
+      secondary: colors.pink,
+    },
   },
   plugins: [],
 }
