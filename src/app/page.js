e88448b06f69ff7a1418e0f82f14587f9115965e
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2026-03-20 12:31:33 -0300

    feat: add custom cursor styles and hover effects

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,12 @@ import ProjectsSection from "@/components/ProjectsSection";
 import EmailSection from "@/components/EmailSection";
 import Footer from "@/components/Footer";
 import KnowledgesSection from "@/components/KnowledgesSection";
+import CustomCursor from "@/components/CustomCursor";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <main className="flex min-h-screen flex-col bg-[#121212] cursor-none">
+      <CustomCursor />
       <Navbar/>
       <div className="container mx-auto px-12 py-4">
         <HeroSession/>
